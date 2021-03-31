47505b6eb33d701b57bf550866b6ad769d683f36
bypolinafox <polina20012012@yandex.ru>
2021-03-31 14:11:21 +0500

    Errors handling

--- sntp-server.py
+++ sntp-server.py
@@ -1,6 +1,7 @@
 import socket
 import struct
 import sys
+import errno
 
 offset = int(sys.argv[1])
 server = 'time.windows.com'
@@ -9,10 +10,32 @@ server = 'time.windows.com'
 def getNormalTime(server):
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     data = '\x1b' + 47 * '\0'
-    client.sendto(data.encode('utf-8'), (server, 123))
-    data, address = client.recvfrom(1024)
-    t_secs = struct.unpack('!12I', data)[10]
-    return (t_secs, data[36:40])
+    client.settimeout(5)
+    try:
+        client.sendto(data.encode('utf-8'), (server, 123))
+        data, address = client.recvfrom(1024)
+        t_secs = struct.unpack('!12I', data)[10]
+        return (t_secs, data[36:40])
+    except socket.error as error:
+        if error.errno == errno.ECONNREFUSED:
+            print("Не самолет, не птица — летит и матерится. "
+                  "Знаешь, про кого это? Про техника ростелекома, "
+                  "который, возможно, где-то накосячил и ты так и не "
+                  "смог узнать время с разницей в " + offset + " секунд")
+
+        elif error.errno == errno.ETIMEDOUT:
+            print("https://shutniki.club/wp-content/uploads/2019/12/hatiko_mem_19_05120800-600x325.png")
+
+        elif error.errno == errno.EADDRNOTAVAIL:
+            print("Адрес, с которого запрашивается время, временно недоступен")
+
+        else:
+            print("Что-то пошло не так. "
+                  "Можешь пока поиграть в "
+                  "https://www.geoguessr.com/ ")
+
+        sys.exit(0)
+
 
 
 def getNTPResponse(req: bytes, offset: int, server: str):
